ff0ececdd01aba3407ff46faa10bc27d7dec3e86
Magnus Skog <magnus.m.skog@gmail.com>
2019-08-13 12:29:37 +0200

    Updated dependencies, changed example to duck duck go

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  const puppeteer = require('puppeteer');
-  (async() => {
-
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+app.get("/", function(req, res) {
+  const puppeteer = require("puppeteer");
+  (async () => {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
     const page = await browser.newPage();
-    await page.goto('https://google.com', {waitUntil: 'networkidle'});
-    // Type our query into the search bar
-    await page.type('test');
-
-    await page.click('input[type="submit"]');
-
-    // Wait for the results to show up
-    await page.waitForSelector('h3 a');
+    await page.goto("https://duckduckgo.com/", {
+      waitUntil: "networkidle2"
+    });
+    await page.type("#search_form_input_homepage", "Puppeteer");
+    await page.click("input[type=submit]");
+    await page.waitForSelector(".result__a");
 
     // Extract the results from the page
     const links = await page.evaluate(() => {
-      const anchors = Array.from(document.querySelectorAll('h3 a'));
+      const anchors = Array.from(document.querySelectorAll(".result__a"));
       return anchors.map(anchor => anchor.textContent);
     });
-    browser.close();
-    res.send(links.join('\n'));
+    await browser.close();
+    res.send(links.join("\n"));
   })();
-})
+});
 
-app.listen(5000, function () {
-})
+app.listen(5000, function() {});
